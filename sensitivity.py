b7dd336f9c95f9739d829e8f2b973e48cab3d990
manuelmarcano22 <manuelmarcano22@yahoo.com>
2015-08-06 14:26:21 +0100

    Update sensitivity.py

--- sensitivity.py
+++ sensitivity.py
@@ -62,6 +62,7 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
         aorb0 = (G*M/om0**2)**(1./3.)       # Kepler's 3rd law
         vorb0 = aorb0*om0                   # orbital velocity in cms
         borb0 = vorb0/c                     # v/c
+        #(2.19 en adV Detector)
         tchirp = 5./(256.*(Mu/M))*(G*M/c**3)/borb0**8   # chirp time
         return tchirp
     
@@ -91,13 +92,18 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
         fp, fc = lal.ComputeDetAMResponse(response, ra, dec, psi, gmst_rad)
         return fp, fc  
 
+
+
     if det == 0:
         det = 'H1'
     if det == 1:
         det = 'L1'
+    if det == 2.:
+        det = 'H1'
     
     apt, act = antenna_response(t0, np.radians(ra), np.radians(dec), psi, det)
     htheory = htheory(m1inj,m2inj,dist,ftheory)
+    
     # get GW strain at detector
     h = htheory* np.sqrt(apt**2 + act**2)
 
@@ -164,85 +170,56 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
                 srm_trans,
                 ds, zeta, eta)
         quantum = np.append(quantum, temp)
-        #quantum = np.sqrt(quantum)
-    #quantum[-5:] = 0.  ####Porque?????
 
 #ASD 
     asd = np.sqrt(seis**2 + coat**2 + susp**2 + quantum)
-    #asd = np.sqrt(seis**2 + coat**2 + susp**2 )
 
-
-    
-    #Interpolation:
-    #hinter = interpolate.interp1d(ftheory, h)
-    #asdinter = interpolate.interp1d(ftheory, asd[:len(h)])
-    #xnew = np.linspace(min(ftheory), max(ftheory), 50000)
-    #hinter = hinter(xnew)
-    #asdinter = asdinter(xnew)
+#Extra ASD:
+#    for i in np.arange(len(freqrange)):
+#        asd[i] = lalsimulation.SimNoisePSDAdvVirgo(freqrange[i])
+#        asd[i] = lalsimulation.SimNoisePSDaLIGONSNSOpt(freqrange[i])
+#    asd = np.sqrt(asd)
 
     # get the SNR
-    #idx = np.argwhere(np.isclose(hinter,asdinter[len(h)])).reshape(-1) 
     freqidx = freqrange[freqrange < 100]
     hidx =  h[0:len(freqidx)]
     idx = abs(abs(hidx*np.sqrt(freqrange[:len(hidx)])) - abs(asd[:len(hidx)]))
-    #idx = np.where(np.sort(idx)==idx)
-    #idx = idx[0:50]
-#    idx = np.sort(idx)
-#    idx = np.where(idx == idx[-1])[0][0]
     idx = np.argmin(idx)
 
-##    idxtemp=np.array([])
-#    for i in range(len(h)-1):
-#        xnew = np.linspace(ftheory[i],ftheory[i+1],1000)
-#        yinterph = np.array(np.interp(xnew, ftheory[i:i+1], h[i:i+1]))
-#        yinterpasd = np.array(np.interp(xnew, ftheory[i:i+1], asd[i:i+1]))
-#        idx2 = np.where(yinterpasd == yinterph)
-#        if np.sum(idx2) > 0:
-#            np.append(idxtemp,i)
-#    print idxtemp
-#
-#
-##    
-#    idx2 = abs(abs(h*np.sqrt(freqrange[:len(h)])) - abs(asd[:len(h)]))
-#    idx2=zip(*[freqrange,idx2])
-#    idx2desde = idx2[idx2[:,0]>1000]
-#    idx2desde = np.sort(idx2desde, axis=1) 
-#    
-#    
-    idx2 = 0
-    desde = 200.
-    if max(ftheory) > desde:
-        freqidx2 = ftheory[ftheory > desde]
-        desdeidx2 = np.where( ftheory == freqidx2[0])[0][0]
-        hastaidx2 = np.where(ftheory == freqidx2[-1])[0][0]
-        #print 'freqdedsde', ftheory[desdeidx2], ftheory[hastaidx2]
-        hidx2 =  h[desdeidx2:hastaidx2]
-        idx2 = abs(abs(hidx2*np.sqrt(ftheory[desdeidx2:hastaidx2])) - abs(asd[desdeidx2:hastaidx2]))
-        idx2 = np.argmin(idx2)
-        #print 'idx2, freq, idx1', idx2, freqrange[idx2], idx
-    #prine idx
-    #print 'h , asd', freqrange[idx],h[idx],asd[idx] 
-    #print idx[0:10]
-    #cercano = min(h, key=lambda x:abs(x-value))
-    #idx = np.where(hinter==asdinter)
-    #i=  idx#np.where(freqrange > 15 )
-    #print i
-    #idx=1
-    hwhite = h[idx:]/asd[idx:len(h)] # whiten waveformi
+    idx2 = len(h)-1 
+    if  fmaxhcode > 100:
+        freqidx2 = freqrange[freqrange > 100]
+        idx2 = np.where(freqrange == freqidx2[0])[0][0]
+        temp = abs(abs(h[idx2:]*np.sqrt(freqrange[idx2:len(h)])) - abs(asd[idx2:len(h)]))
+        temp = np.argmin(temp)
+        idx2 = temp + idx2
+
+#For the Horizon Distance
+    hwhite1 = htheory/asd[:len(htheory)] # whiten waveformi
+    #deltaF = freqrange[2]-freqrange[1]
+    deltaF =1.  #######PReguntar por esto
+    snr1 = np.sqrt(4.*deltaF*np.vdot(hwhite1, hwhite1).real)
+
+    hwhite = h/asd[:len(h)] # whiten waveformi
+    #hwhite = h[idx:]/asd[idx:len(h)] # whiten waveformi
     deltaF = freqrange[2]-freqrange[1]
-    #print deltaF
+    deltaF = 1.
     snr = np.sqrt(4.*deltaF*np.vdot(hwhite, hwhite).real)
 
-    timeelapsed = abs(tchirp(m1inj,m2inj,ftheory[0])  - tchirp(m1inj,m2inj,ftheory[-1]))
 
+    timeelapsed = abs(tchirp(m1inj,m2inj,ftheory[idx])  - tchirp(m1inj,m2inj,ftheory[idx2]))
+    
+    time_to_ISCO =  tchirp(m1inj,m2inj,ftheory[-1])        
+    BNS_horizon = snr1/8. *dist
+    ave_range = BNS_horizon / 2.26
     #print 'i %d' %i
     #print 'mm %d' %mm
 
     return snr, h, asd, freqrange, ftheory, timeelapsed,seis, coat, \
-            susp, fGWI, idx, idx2, quantum
-#
-###
-##Prueba:s
+            susp, fGWI, idx, idx2, quantum, time_to_ISCO, BNS_horizon, ave_range
+##
+####
+##To test the routine. 
 ##To call the FUnctions:
 #t0=900000000 #gps seconds
 #iota = 0. #(radians) Inclination
@@ -253,28 +230,30 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
 #m1inj = 1
 #m2inj = 1
 #det =0
-#fmaxhcode = 5000000.
-#massdet = 80.
+#fmaxhcode =  20 #5000000.
+#massdet = 40.
 #ltotal =  100*2.14 # 1.6#1.87# 2.14 #1.87   # 2.1
 #lsec = 100*1.1 #0.6
 #
 #snr1, h1, asd1, freqrange1, freqrangetheory, timee1, \
-#        seis1, coat1, susp1, fGWI ,idx,idx2, quantum1= \
+#        seis1, coat1, susp1, fGWI ,idx,idx2, quantum1, tisco= \
 #        sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal,lsec)
-##for i in np.arange(40,120,20):
-##        snr1, h1, asd1, freqrange1, freqrangetheory, timee1, \
-##            seis1, coat1, susp1, fGWI ,idx,idx2, quantum1= \
-##            sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,i,ltotal,lsec)
-##        print i
-##        print quantum1[-10:]
-##        print freqrange1[-1]
 #
+##data = np.column_stack((freqrange1,quantum1))
+###for i in np.arange(40,120,20):
+###        snr1, h1, asd1, freqrange1, freqrangetheory, timee1, \
+###            seis1, coat1, susp1, fGWI ,idx,idx2, quantum1= \
+###            sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,i,ltotal,lsec)
+###        print i
+###        print quantum1[-10:]
+###        print freqrange1[-1]
+##
 #figure = plt.figure()
 ###
 #subplot1 = figure.add_subplot(111)
 #
 #inset_ax = figure.add_axes([0.55,0.04,0.45,0.3] )
-#mapita = Basemap(projection='moll',lon_0 = 10)
+#mapita = Basemap(projection='moll',lon_0 = 0)
 ##, projection =ccrs.PlateCarree())
 ####
 #subplot1.set_yscale('log')
@@ -289,7 +268,7 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
 #        bbox = dict(boxstyle='round', ec=(1., 0.5, 0.5),\
 #                    fc=(1., 0.8, 0.8),))
 #
-#subplot1.set_ylim([1e-26, 1e-10])
+#subplot1.set_ylim([1e-27, 1e-17])
 #subplot1.set_xlim([1, 2e4])
 #
 #subplot1.grid(True, which="both", ls="-", alpha=.5)
@@ -330,7 +309,7 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
 #
 #try:
 #    subplot1.scatter(freqrangetheory[idx],h1[idx]*np.sqrt(freqrangetheory[idx]),c='r',s=500,marker='*')
-#    subplot1.scatter(freqrangetheory[idx2],h1[idx2]*np.sqrt(freqrangetheory[idx2]),color='b',s=500)
+#    subplot1.scatter(freqrangetheory[idx2],h1[idx2]*np.sqrt(freqrangetheory[idx2]),color='b',s=100)
 #except:
 #    print 'no se pudo'
 ##mapita.drawcoastlines()
@@ -369,7 +348,7 @@ def sensitivity(t0,ra,dec,iota,psi,dist,m1inj,m2inj,det,fmaxhcode,massdet,ltotal
 #    return ft
 #t0=900000000 #gps seconds
 #psi = 0#0.343 #(radians) Polarization angle
-#det = 'L1'
+#det = 'H1'
 #
 #
 #Xlista = np.arange(0,360.,1.)
